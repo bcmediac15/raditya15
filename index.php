171be3e20572adbcda54c6211b06fe89fd91f85c
bcmediac15 <made@artha.web.id>
2020-08-05 06:55:14 +0800

    Update Judul Page
    Signed-off-by: bcmediac15 <made@artha.web.id>

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="fontawesome/css/all.css" rel="stylesheet">
 
-    <title>Info Sekolah SMPN.2 Kuta Selatan </title>
+    <title>Info Sekolah SMPN.2 Kuta Selatan - Keperluan Pribadi</title>
     <style>
         html {
             scroll-behavior: smooth;
